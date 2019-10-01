fe49ace0230e0110ac01597e8b2465114fa5a593
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-01 14:43:21 -0700

    inital commit

---
+++ test/app.test.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+});
